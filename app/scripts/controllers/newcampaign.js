c0e442cbd6723fc42b2cc8c0d0c0c736d662bf5f
metalrockdanclex <metalrockdanclex@mail.ru>
2014-11-13 09:12:24 +0100

    new campaign html updated

--- app/scripts/controllers/newcampaign.js
+++ app/scripts/controllers/newcampaign.js
@@ -3,16 +3,21 @@ angular.module('thingie').controller('newCampaignCtrl', function($scope, api)
  {
   'use strict';
 
-
+$scope.NewCampaignData = 
+    {
+      'id': '',
+      'Name': '',
+      'Type': '',
+      'MinAge': '',
+      'MaxAge': '',
+      'Description': '',
+      'BeginDate': '',
+      'EndDate': '',
+      'Interests': '',
+      'Gender': ''
+    };
 
         // need to wait for the login promise
-      api.campaigns().then(function(data) 
-      {
-        $scope.activecampaigns = data;
-        $scope.activecampaignscount = data.length;
-      });
-
-
 });
 
 
