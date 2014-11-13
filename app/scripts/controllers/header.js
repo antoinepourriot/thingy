73802154780254d886b976c74fc487eaa82b3b49
Antoine Pourriot <antoine.pourriot@gmail.com>
2014-11-13 03:30:12 +0100

    highlight menu items

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,8 +1,16 @@
-angular.module('thingie').controller('headerCtrl', function($scope, $route) {
+angular.module('thingie').controller('headerCtrl', function($scope, $route, $location) {
   'use strict';
 
   $scope.refresh = function() {
     $route.reload();
+  };
+  // highlight tab based on view location
+  $scope.getClass = function(path) {
+    if ($location.path().substr(0, path.length) === path) {
+      return 'active';
+    } else {
+      return '';
+    }
   };
 
 });
